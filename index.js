7d60d94e433461b337a55dddc62cd34167f1f6e7
Ryisan Champion <44957246+ryisan2@users.noreply.github.com>
2024-05-20 22:50:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const scaleFactor =1/20; //this is the scale factor for the movement of the shapes
+const scaleFactor = 1 / 20;
 
 function moveBg(event) {
-  const shapes = document.querySelectorAll(".shape")
-  const x = event.clientX * scaleFactor; 
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
   const y = event.clientY * scaleFactor;
-  
-  for ( let i = 0; i < shapes.length; i++) { //we loop through the shapes and apply the transform property to each shape
-  const isOdd = i % 2 !== 0;
-  const boolInt= isOdd ? 1 : -1; //we check if the index is odd or even and apply the oddInt to the transform property
-  shapes[i].style.transform = `translate(${x * boolInt}px, ${y*boolInt}px)` //we apply the transform property to the shape
-  console.log(x, y)
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
   }
 }
-
 
 const movieEl = document.querySelector(".movie__wrapper");
 
@@ -33,67 +31,93 @@ async function onSearchChange(event) {
 
 function movieHTML(movie) {
   return `
-    <div class="movie">
-      <div class="movie__content">
-        <img src="${movie.Poster}" alt="" class="movie__img" onclick='toggleModal(${JSON.stringify(movie).replace(/'/g, "\\'").replace(/"/g, '&quot;')})'>
-        <div class="movie__info">
-          <div class="more-info">
-            <h1>${movie.Title}</h1>
-            <h1>${movie.Year}</h1>
-            <p><a onclick="movieInfo('${movie.imdbID}')">SEE MORE</a></p>
+      <div class="movie" >
+        <div class="movie__content click__me" data-movie='${JSON.stringify( movie )}' onclick='toggleModal(this)'>
+          <img src="${movie.Poster}" alt="" class="movie__img" >
+          <div class="movie__info">
+            <div class="more-info">
+              <h1>${movie.Title}</h1>
+              <h1>${movie.Year}</h1>
+              <p><a onclick="movieInfo('${
+                movie.imdbID
+              }')">Click to SEE MORE</a></p>
+            </div>
           </div>
         </div>
-      </div>
-    </div>`;
+      </div>`;
 }
 
 let isModalOpen = false;
 
-// function toggleModal(movie = null) {
-//   isModalOpen = !isModalOpen;
+function toggleModal(clickedElement, event) {
+  // If the event object is provided, stop the propagation
+  if (event) {
+    event.stopPropagation();
+  }
 
-//   if (isModalOpen && movie) {
-//     modalInfo(movie);
-//   } else {
-//     document.body.classList.remove("modal__open");
-//   }
-// }
+  const modal = document.querySelector(".modal");
 
-// function modalInfo(movie) {
-//     document.body.classList.add("modal__open"); 
-//   // Update the left side of the modal with the movie name and poster
-//   const modalLeft = document.querySelector('.modal__half-left');
-//   modalLeft.innerHTML = `
-//     <div class="information intro__txt2">
-//       <h3><span class="modal__title modal__title--about">${movie.Title}</span></h3>
-//       <img src="${movie.Poster}" alt="${movie.Title}" class="movie__img">
-//     </div>
-//   `;
+  // Check if the clicked element has the data attribute for movie
+  if (clickedElement && clickedElement.dataset.movie) {
+    const movie = JSON.parse(clickedElement.dataset.movie);
+    modalInfo(movie); // Make sure this function is defined and works correctly
+    modal.classList.add("modal__open");
+    isModalOpen = true;
+  } else {
+    modal.classList.remove("modal__open");
+    console.log("closing modal");
+    isModalOpen = false;
+  }
+}
 
-//   // Update the right side of the modal with the movie information
-//   const modalRight = document.querySelector('.modal__half-right');
-//   modalRight.innerHTML = `
-//     <h3 class="contact__header"><span>${movie.Title}</span></h3>
-//     <br>
-//     <h4 class="contact__sub">${movie.Rated}</h4>
-//     <h4 class="contact__sub">${movie.Rated}</h4>
-//     <p>${movie.Plot}</p>
-//     <div class="btn__div">
-//       <button id="contact_submit" class="contact__btn click__me" type="submit">
-//         Purchase Tickets!
-//         </div>
-//         <div> <i class="fas fa-times modal__exit click" onclick="toggleModal()"></i> </div>
-//     </div>
-//       </button>
-//     </div>
-//   `;
 
-// }
+function modalInfo(movie) {
+  document.body.classList.add("modal__open");
+  // Update the left side of the modal with the movie name and poster
+  const modalLeft = document.querySelector(".modal__half-left");
+  modalLeft.innerHTML = `
+    <div class="information intro__txt2">
+      <h3><span class="modal__title modal__title--about">${movie.Title}</span></h3>
+      <img src="${movie.Poster}" alt="${movie.Title}" class="movie__img">
+    </div>
+  `;
 
-// function movieInfo(imdbID) {
-//   localStorage.setItem("imdbID", imdbID);
-//   window.location.href = "movie.html";
-// }
+  // Update the right side of the modal with the movie information
+  const modalRight = document.querySelector(".modal__half-right");
+  modalRight.innerHTML = `
+    <h3 class="contact__header"><span>${movie.Title}</span></h3>
+    <br>
+    <h4 class="contact__sub">${movie.Rated}</h4>
+    <h4 class="contact__sub">${movie.Rated}</h4>
+    <p>${movie.Plot}</p>
+    <div class="btn__div">
+      <button id="contact_submit" class="contact__btn click__me" type="submit">
+        Purchase Tickets!
+        </div>
+        <div > 
+        <i class="fas fa-times modal__exit click__me" onclick="closeModal()" ></i> 
+      </div>
+          </div>
+      </button>
+    </div>
+  `;
+}
+
+function closeModal() {
+  document.body.classList.remove("modal__open");
+}
+
+// Close the modal when the user clicks outside of it
+document.addEventListener("click__me", function (event) {
+  if (isModalOpen) {
+    toggleModal();
+  }
+});   
+
+function movieInfo(imdbID) {
+  localStorage.setItem("imdbID", imdbID);
+  window.location.href = "movie.html";
+}
 
 function toggleContrast() {
   document.body.classList.toggle("dark-theme");
